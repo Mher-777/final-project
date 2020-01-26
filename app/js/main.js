69c6f14a1c040883c5a4441fb90bd0e188fc5b23
Mher-777 <mherjan2003@mail.ru>
2020-01-26 22:57:16 +0400

    added header and main-slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,12 @@
 $(function () {
-    
-    
+
+    $('.main-slider__inner').slick({
+        rows: 0,
+        prevArrow: '<button class="main-arrow main-arrow__left"></button>',
+        nextArrow: '<button class="main-arrow main-arrow__right"></button>',
+        fade: true,
+        dots: true,
+        dotsClass: 'main-dots',
+    })
 
 });
