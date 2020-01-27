3a8c57e89de3b577ac41ac73937588dc6faf6704
Mher-777 <mherjan2003@mail.ru>
2020-01-27 16:32:23 +0400

    added api slider

--- app/js/main.js
+++ app/js/main.js
@@ -7,6 +7,64 @@ $(function () {
         fade: true,
         dots: true,
         dotsClass: 'main-dots',
+    });
+    $('.product__slider').slick({
+        rows: 0,
+        arrows: false,
+        dots: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        centerMode: true,
+        variableWidth: true,
+        asNavFor: '.product-pagination__slider',
+
+    });
+    $('.product-pagination__slider').slick({
+        rows: 0,
+        dots: false,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        asNavFor: '.product__slider',
+        focusOnSelect: true,
+        arrows: true,
+        prevArrow: '<button class="product-arrows product-arrows__left"></button>',
+        nextArrow: '<button class="product-arrows product-arrows__right"></button>',
+    });
+
+    $.ajax({
+        url: "https://jsonplaceholder.typicode.com/photos",
+        method: "GET",
+        dataType: "JSON",
+        success: (data) => {
+
+            for (let i = 0; i < 10; i++) {
+                $(".product__slider").append(
+                    `
+                        
+                            <div class="product__slider-item">
+                <div class="product__slider-item-images">
+                    <img src="${data[i]['url']}" alt="">
+                    <div class="product__slider-item-head">
+                        <a class="product__slider-item-head-link"></a>
+                        <a class="product__slider-item-head-link"></a>
+                        <a class="product__slider-item-head-link"></a>
+                        <a class="product__slider-item-head-link"></a>
+                    </div>
+                </div>
+                <div class="product__slider-item-content">
+                    <div class="product__slider-item-box">
+                        <div class="product__slider-item-star"></div>
+                        <div class="product__slider-item-title">Jack chair - Black velvet</div>
+                    </div>
+                    <a href="#" class="product__slider-item-price">
+                        $ 126
+                    </a>
+                </div>
+            </div>
+                       `
+                )
+            }
+        }
     })
 
 });
