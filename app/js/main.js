81a7ac3424de9d30f4b029ce9940463b61820c4e
Mher-777 <mherjan2003@mail.ru>
2020-01-31 15:37:04 +0400

    added hover function

--- app/js/main.js
+++ app/js/main.js
@@ -69,6 +69,7 @@ $(function () {
         infinite: false,
         autoplay: true,
     });
+
     $('.blog__content-slider').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
@@ -78,30 +79,84 @@ $(function () {
 
     wow = new WOW(
         {
-            offset:       150,
+            offset: 150,
         }
     );
 
     $('.our-gallery__item').slice(0, 8).show();
+
     $('.button__btn').on('click', function (e) {
         e.preventDefault();
         $('.our-gallery__item:hidden').slice(0, 8).slideDown();
     });
+
+    $('.register-btn').on('click', function () {
+        $('.header__top-register').toggleClass('active');
+        $('.header__top-log').removeClass('active')
+    });
+    $('.login-btn').on('click', function () {
+        $('.header__top-log').toggleClass('active');
+        $('.header__top-register').removeClass('active')
+    });
+
     wow.init();
     let mixer = mixitup('.categories__inner');
-    // $.ajax({
-    //     url: 'https://jsonplaceholder.typicode.com/photos',
-    //     data: 'JSON',
-    //     type: 'GET',
-    //     success: function (data) {
-    //         for (let i = 0; i < 1; i++) {
+
+
+    let textOne = $('.header-personal__item.icon-hart').text();
+    let textTwo = $('.header-personal__item.icon-cart').text();
+
+    function textRemove (){
+        $('.number-one').text('(' + textOne + ')');
+        $('.number-two').text('(' + textTwo + ')');
+    }
+    textRemove ();
+
+    let search = $('.header__bottom-search');
+    let searchContent = $('.header__bottom-search-content');
+
+    $(search).on('click', function () {
+        $(searchContent).css('display', 'block');
+    });
+
+    $('.header__bottom-search-content.active').click(function(){
+        $(this).addClass('active')
+    });
+
+
+
+
+
+
+
+
+
+
+    // $('.test').on('click', function () {
+    //     $.ajax({
+    //         url: 'http://localhost:8011/rest/users/',
+    //         data: 'JSON',
+    //         type: 'POST',
+    //         success: function (data) {
     //             console.log(data);
-    //             $('.product__slider-item-images').append(
-    //                 `<img src="https://via.placeholder.com/150/92c952" alt="">`
-    //             )
     //         }
-    //     }
+    //     })
     // })
 
+    // var obj = { name: "John", age: 30, city: "New York" };
+    //
+    // $('#header-btn__reg').on('click', function () {
+    //     $.ajax({
+    //         url: 'http://localhost:8011/rest/users/',
+    //         dataType: 'JSON',
+    //         data: JSON.stringify(obj),
+    //         type: 'POST',
+    //         contentType: "application/json;charset=utf-8",
+    //         success: function (data) {
+    //             console.log(data);
+    //         },
+    //
+    //     })
+    // })
 
 });
